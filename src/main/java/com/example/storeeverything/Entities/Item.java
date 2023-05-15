9312da8ea074676f0ee6a9b677ba08383608e5fd
marcin15121 <marcin15121@hotmail.com>
2023-05-15 13:58:49 +0200

    Bootstrap

--- src/main/java/com/example/storeeverything/Entities/Item.java
+++ src/main/java/com/example/storeeverything/Entities/Item.java
@@ -18,18 +18,23 @@ public class Item {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private long id;
+
     @Column(name = "title",  length = 20, nullable = false)
     private String title;
+
     @Column(name = "content", length = 500, nullable = false)
     private String content;
-    @Column(name = "url", length = 100, nullable = true)
 
+    @Column(name = "url", length = 100, nullable = true)
     private String url;
+
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "category", nullable = false)
     private Category category;
+
     @Column(name = "created", nullable = false)
     private LocalDate created;
+
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user", nullable = false)
     private User user;
--- src/main/java/com/example/storeeverything/Services/ItemService.java
+++ src/main/java/com/example/storeeverything/Services/ItemService.java
@@ -1,5 +1,6 @@
 package com.example.storeeverything.Services;
 
+import com.example.storeeverything.Entities.Category;
 import com.example.storeeverything.Entities.Item;
 import com.example.storeeverything.Repositories.CategoryRepository;
 import com.example.storeeverything.Repositories.ItemRepository;
@@ -13,7 +14,13 @@ import java.util.Optional;
 @Service
 public class ItemService {
     @Autowired
-    ItemRepository items;
+    ItemRepository itemRepository;
 
-    
+    public List<Item> getAllItems(){return  itemRepository.findAll();}
+
+    public void addNewItem(Item item){
+        itemRepository.save(item);
+    }
+
+    public void deleteItem(Long id) {itemRepository.deleteById(id);}
 }
