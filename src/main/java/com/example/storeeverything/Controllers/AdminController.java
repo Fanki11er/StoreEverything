f57d3661ca2dcceaa4f792615d3a7c3273e0435d
ewela <ewela28@poczta.onet.pl>
2023-05-24 00:15:27 +0200

    Started adding changing user role by admin

--- src/main/java/com/example/storeeverything/Controllers/AdminController.java
+++ src/main/java/com/example/storeeverything/Controllers/AdminController.java
@@ -1,12 +1,14 @@
 package com.example.storeeverything.Controllers;
 import com.example.storeeverything.Dtos.UserDto;
+import com.example.storeeverything.Dtos.NewRoleDto;
+import com.example.storeeverything.Role;
 import com.example.storeeverything.Services.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/App/Admin")
@@ -19,9 +21,26 @@ public class AdminController {
     public  String getUserInfo(@RequestParam Long id, Model model){
         UserDto user = userService.loadLUserById(id);
         String loggedUserRole = userService.getLoggedUserRole();
+        ArrayList<String> availableRoles = new ArrayList<>();
+
+        for (int i=0; i < Role.values().length; i++) {
+            if(!Role.values()[i].name().equals(loggedUserRole)){
+                availableRoles.add(Role.values()[i].name());
+            }
+        }
+
         model.addAttribute("user", user);
         model.addAttribute("path", "User");
         model.addAttribute("loggedUserRole", loggedUserRole);
+        model.addAttribute("roles", availableRoles);
+        model.addAttribute("newRole", new NewRoleDto());
         return "editUser";
     }
+
+    @PostMapping("/User/Edit")
+    public String editUserRole(@RequestParam Long id, @ModelAttribute("newRole") NewRoleDto newRole){
+        System.out.println(newRole.getNewRole());
+        return "redirect:/App/Admin/User?id=" + id;
+    }
+
 }
---
+++ src/main/java/com/example/storeeverything/Dtos/NewRoleDto.java
@@ -0,0 +1,12 @@
+package com.example.storeeverything.Dtos;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class NewRoleDto {
+    private String newRole;
+}
